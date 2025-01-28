1cb9d381a4be72fa1cf7c5515de8eeb7d249a70e
Kate Makarova <kate@donkeymob.com>
2025-01-28 10:48:32 -0500

    src

---
+++ lib/sceditor/sceditor.component.d.ts
@@ -0,0 +1,12 @@
+import { OnInit } from '@angular/core';
+import * as i0 from "@angular/core";
+export declare class SceditorComponent implements OnInit {
+    id: string;
+    format: string;
+    private scripts;
+    private scriptService;
+    constructor();
+    ngOnInit(): void;
+    static ɵfac: i0.ɵɵFactoryDeclaration<SceditorComponent, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<SceditorComponent, "app-sceditor", never, { "id": { "alias": "id"; "required": false; }; "format": { "alias": "format"; "required": false; }; }, {}, never, never, true, never>;
+}

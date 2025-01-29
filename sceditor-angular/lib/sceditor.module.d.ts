2eb6cc68c20db1cc16c2c12f0299919353289f7d
kate makarova <kate.makarova@hotmail.com>
2025-01-29 15:42:35 +0000

    Update compiled code

---
+++ sceditor-angular/lib/sceditor.module.d.ts
@@ -0,0 +1,9 @@
+import * as i0 from "@angular/core";
+import * as i1 from "@angular/common";
+import * as i2 from "./sceditor/sceditor.component";
+export declare class SCEditorModule {
+    static getValue(id: string): string;
+    static ɵfac: i0.ɵɵFactoryDeclaration<SCEditorModule, never>;
+    static ɵmod: i0.ɵɵNgModuleDeclaration<SCEditorModule, never, [typeof i1.CommonModule, typeof i2.SceditorComponent], [typeof i2.SceditorComponent]>;
+    static ɵinj: i0.ɵɵInjectorDeclaration<SCEditorModule>;
+}
